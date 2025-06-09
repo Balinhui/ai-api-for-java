308c962fb5b489c5ce00197dfe1b1f3fd71b7c97
Balinhui <balinhui2023@gmail.com>
2025-06-09 13:37:07 +0800

    upgrade( LogFile): 新增方法createFile

--- src/main/java/org/balinhui/util/LogFile.java
+++ src/main/java/org/balinhui/util/LogFile.java
@@ -14,12 +14,19 @@ public class LogFile {
     protected LogFile() {}
 
     static {
-        if (!logDirectory.isDirectory()) {
-            if (!logDirectory.mkdir()) throw new RuntimeException("无法创建文件夹");
-        }
+        createDir();
     }
 
     public static @NotNull String[] getLogList() {
         return logDirectory.list() == null ? new String[0] : logDirectory.list();
+    }
+
+    public static void createDir() {
+        if (!logDirectory.exists()) {
+            if (!logDirectory.mkdir()) throw new RuntimeException("无法创建文件夹");
+        }
+        if (!logDirectory.isDirectory()) {
+            throw new RuntimeException("history不是文件夹而是文件，请把名为history的文件移动至别处或删除");
+        }
     }
 }
